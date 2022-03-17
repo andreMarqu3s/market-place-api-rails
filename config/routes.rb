5329390881cfcd169a2d7f79f022e19c7bfb2f09
André Marques <markues@protonmail.com>
2022-03-17 20:39:16 -0300

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # api
-  namespace :api, defaults: { format: json } do
+  namespace :api, defaults: { format: JSON } do
     namespace :v1 do
       # resources
     end
