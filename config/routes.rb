c2371f94f0ed2fcf88901627fc7ff2edd7fce219
André Marques <markues@protonmail.com>
2022-03-18 12:28:15 -0300

    add update action in user controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: JSON } do
     namespace :v1 do
       # resources
-      resources :users, only: %i[show create]
+      resources :users, only: %i[show create update]
     end
   end
 end
