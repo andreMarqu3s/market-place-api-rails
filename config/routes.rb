63142d6e15ba61b92efe85d51b103ca3a0dde0c1
André Marques <markues@protonmail.com>
2022-03-18 11:40:52 -0300

    add user create endpoint and tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: JSON } do
     namespace :v1 do
       # resources
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
     end
   end
 end
