a78f2e70ce54f1ef35c1359876334fba7c7c66cb
André Marques <markues@protonmail.com>
2022-03-18 20:52:14 -0300

    setup JWT gem

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +11,7 @@ module MarketPlaceApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.eager_load_paths << Rails.root.join('lib')
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
